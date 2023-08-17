97f10ea296ce36b71021d683640d77324b91e4bc
eckong <eckong@abonaro.com>
2023-08-17 13:48:57 +0900

    0.0.4

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    jest: "readonly",
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "import"],
-  ignorePatterns: ["dist/", "coverage/", "bin/", "build/", "logs/"],
+  plugins: ["@typescript-eslint"],
   rules: {
     "no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "off",
